f52c95253d66a40f06e0b2034f06e187fc11e177
Gustavo Merling <gustavo@voltsdigital.com.br>
2016-07-20 08:16:35 -0300

    SXPL Site

---
+++ sxpl/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sexy Pearl</title>
+    <meta name="description" content="Banda de hard rock autoral joinvilense.">
+    <meta name="author" content="Sexy Pearl">
+
+    <link rel="stylesheet" href="css/normalize.css?v=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/fullpage.css?v=1.0">
+    <link rel="stylesheet" href="css/style.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <header>
+        <div class='center full_height'>
+            <a class='logo' href='#home'>Sexy Pearl</a>
+            <ul id='menu'>
+                <li data-menuanchor='contact'><a href='#contact' class='header_link'>Contato</a></li>
+                <li data-menuanchor='about'><a href='#about' class='header_link'>Sobre</a></li>
+                <li data-menuanchor='home'><a href='#home' class='header_link'>Home</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <div id="fullpage">
+        <div class="section home"></div>
+        <div class="section about"></div>
+        <div class="section contact"></div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    <script src="js/fullpage.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
