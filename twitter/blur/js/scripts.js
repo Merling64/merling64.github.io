b7abd83a2866463f8bafbf919c0dc6da20a21cf5
Gustavo Merling <Gustavo Merling>
2016-10-16 19:16:50 -0200

    ..

--- twitter/blur/js/scripts.js
+++ twitter/blur/js/scripts.js
@@ -4,6 +4,11 @@
 
 $(document).ready(function(){
 
-	
+	function longClickHandler(e){
+		e.preventDefault();
+		$("body").append("<p>You longclicked. Nice!</p>");
+	}
+
+	$("p a").longclick(250, longClickHandler);
 
 });
